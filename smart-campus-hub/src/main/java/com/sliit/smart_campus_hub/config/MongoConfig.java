bd2dd2a01bc4bf38b06b9b8e1078a807926dda05
SeniruJay <senij2001@gmail.com>
2026-04-26 22:54:04 +0530

    Initial changes

--- smart-campus-hub/src/main/java/com/sliit/smart_campus_hub/config/MongoConfig.java
+++ smart-campus-hub/src/main/java/com/sliit/smart_campus_hub/config/MongoConfig.java
@@ -12,12 +12,12 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${spring.data.mongodb.connection-string}")
+    @Value("${spring.data.mongodb.uri}")
     private String connectionString;
 
     @Override
     protected String getDatabaseName() {
-        return "smart-campus-hub";
+        return "smart_campus";
     }
 
     @Override
